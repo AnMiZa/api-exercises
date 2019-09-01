9d1683e85c85e12a9478e56a2d8e8d0ad9e7201a
Andrzej Zaborski <andrzejzab@gmail.com>
2019-09-01 19:57:43 +0200

    Handled some errors

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const fetch = require("node-fetch")
 
-const year = process.argv[2] || Math.floor(Math.random() * 2020)
+const arg =
+    process.argv[2] ||
+    (console.log("Write something in format --{year, math, trivia}={number}") ||
+        process.exit())
+const equalSign = arg.search("=")
 
-fetch(`http://numbersapi.com/${year}/year?json`)
-    .then(res => res.json())
+const number = arg.slice(equalSign + 1) || Math.floor(Math.random() * 2020)
+
+let type = ""
+
+if (number === "" || isNaN(Number(number))) {
+    console.log("To nie jest liczba")
+    process.exit()
+}
+
+if (equalSign === -1) console.log("You did not specified any number")
+
+if (arg.indexOf("--year") === 0) {
+    console.log("We are searching informations about given year...")
+    type = "year"
+} else if (arg.indexOf("--math") === 0) {
+    console.log("We are searching some math fact about given number...")
+    type = "math"
+} else if (arg.indexOf("--trivia") === 0) {
+    console.log("We are searching some trivia about given number...")
+    type = "trivia"
+}
+
+fetch(`http://numbersapi.com/${number}/${type}?json`)
+    .then(res => {
+        if (res.ok) {
+            return res.json()
+        } else {
+            throw new Error("Something went wrong:" + res.status)
+        }
+    })
     .then(data => console.log(data.text))
     .catch(err => console.log("Error", err))
