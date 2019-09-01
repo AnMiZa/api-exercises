05efab08d58956f55c089d24e39761d3e630946f
Andrzej Zaborski <andrzejzab@gmail.com>
2019-09-01 19:03:03 +0200

    node-fetch installed and used in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch")
 
 const year = process.argv[2]
-console.log(year)
 
-fetch
+fetch(`http://numbersapi.com/${year}/year?json`)
+    .then(res => res.json())
+    .then(data => console.log(data.text))
+    .catch(err => console.log("Error", err))
